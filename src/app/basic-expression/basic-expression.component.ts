ba9129f3000f7cebf02555630fe7fa3f060fc5bf
Junyoung <kensiro5218@gmail.com>
2019-08-19 00:16:26 +0800

    add basic expression

--- src/app/basic-expression/basic-expression.component.ts
+++ src/app/basic-expression/basic-expression.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-basic-expression',
   templateUrl: './basic-expression.component.html',
-  styleUrls: ['./basic-expression.component.scss']
+  styleUrls: ['./basic-expression.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({opacity:0}),
+        animate(500, style({opacity:1})) 
+      ])
+    ])
+  ]
 })
 export class BasicExpressionComponent implements OnInit {
+
+  contentShow:number = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public selectContent(index) {
+    this.contentShow = index;
+  }
 }
