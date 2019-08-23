6408604dfdefc6ae2953af86eba2c86b049b003d
Junyoung <Junyoung.Hwang@ibm.com>
2019-08-23 18:22:00 +0800

    update verb

--- src/app/special-expression/special-expression.component.ts
+++ src/app/special-expression/special-expression.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-special-expression',
   templateUrl: './special-expression.component.html',
-  styleUrls: ['./special-expression.component.scss']
+  styleUrls: ['./special-expression.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({opacity:0}),
+        animate(500, style({opacity:1})) 
+      ])
+    ])
+  ]
 })
 export class SpecialExpressionComponent implements OnInit {
 
--- src/app/verb/verb.component.ts
+++ src/app/verb/verb.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-verb',
   templateUrl: './verb.component.html',
-  styleUrls: ['./verb.component.scss']
+  styleUrls: ['./verb.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({opacity:0}),
+        animate(500, style({opacity:1})) 
+      ])
+    ])
+  ]
 })
 export class VerbComponent implements OnInit {
-
+  
+  contentShow:number = 0;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public selectContent(index) {
+    this.contentShow = index;
   }
 
 }
