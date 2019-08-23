455d27f5e205d48cdd0923b2d5be3291cdff1161
Junyoung <Junyoung.Hwang@ibm.com>
2019-08-23 13:54:26 +0800

    update adverb

--- src/app/adverb/adverb.component.ts
+++ src/app/adverb/adverb.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-adverb',
   templateUrl: './adverb.component.html',
-  styleUrls: ['./adverb.component.scss']
+  styleUrls: ['./adverb.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({opacity:0}),
+        animate(500, style({opacity:1})) 
+      ])
+    ])
+  ]
 })
 export class AdverbComponent implements OnInit {
 
